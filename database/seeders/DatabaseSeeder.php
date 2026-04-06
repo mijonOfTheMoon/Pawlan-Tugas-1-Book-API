420431f55fade3ab5526bc70b6e1639c64c69577
Hasbi Mizan Azzami <devopshasbi@gmail.com>
2026-04-07 01:33:38 +0700

    chore(database): add book seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Book;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,6 +20,20 @@ class DatabaseSeeder extends Seeder
             'name' => 'admin',
             'email' => 'admin@example.com',
             'password' => bcrypt('admin123'),
+        ]);
+
+        Book::create([
+            'title' => 'Laskar Pelangi',
+            'author' => 'Andrea Hirata',
+            'publisher' => 'Bentang Pustaka',
+            'year' => 2005,
+        ]);
+
+        Book::create([
+            'title' => 'Bumi Manusia',
+            'author' => 'Pramoedya Ananta Toer',
+            'publisher' => 'Hasta Mitra',
+            'year' => 1980,
         ]);
     }
 }
